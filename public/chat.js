b0dce5ceec9386bfb0c58fd389cb4e1ee3dc9df9
Jeremie Amstutz <zelod.contact@gmail.com>
2017-10-09 18:28:56 +0200

    Added socket.io

---
+++ public/chat.js
@@ -0,0 +1,51 @@
+// Make connection
+var socket = io.connect('http://128.179.166.16:4000');
+
+// Query DOM
+var message = document.getElementsByClassName('conversation-composer')[0],
+      username = document.getElementsByClassName('header-name-body')[0],
+      usernameIcon = document.getElementsByClassName('header-name-icon')[0],
+      conversation = document.getElementsByClassName('conversation-messages')[0];
+
+
+// Emit events
+var submit = function(){
+
+    // Emit message
+    socket.emit('chat', {
+      message: message.value,
+      username: username.value
+    });
+
+    //Add message to author conversation
+    conversation.innerHTML +=
+    '<div class="conversation-message right"><div class="conversation-message-body right">' + message.value + '</div></div>';
+
+    // Reset input
+    message.value = "";
+};
+
+
+// Listen for a Enter press
+message.addEventListener('keydown', function(e) {
+  // Enter is pressed
+  if (e.keyCode == 13) { submit(); }
+  }, false);
+
+username.addEventListener('keydown', function(e) {
+  // Enter is pressed
+  if (e.keyCode == 13) { changeColorIcon(); }
+  }, false);
+
+
+// Change username icon
+var changeColorIcon = function() {
+  usernameIcon.style.backgroundColor = '#' + username.value;
+};
+
+
+// Listen for events
+socket.on('chat', function(data){
+   conversation.innerHTML +=
+   '<div class="conversation-message"><div class="conversation-message-user-icon" style="background-color: #' + data.username + ';" ></div><div class="conversation-message-body">' + data.message + '</div></div>';
+});
---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var socket = require('socket.io');
+
+// App setup
+var app = express();
+var server = app.listen(4000, function(){
+    console.log('Listening for requests on port 4000...');
+});
+
+// Static files
+app.use(express.static('public'));
+
+// Socket setup & pass server
+var io = socket(server);
+io.on('connection', (socket) => {
+
+    console.log('New connection :', socket.id);
+
+    // Handle chat event
+    socket.on('chat', function(data){
+
+        // Log events
+        console.log(data);
+
+        // Broadcast message
+        socket.broadcast.emit('chat', data);
+    });
+
+});
